2f91aff127848d80fe57e7c860e5edd5cea7d06d
Alex Chatham <Alexchatham@gmail.com>
2012-06-28 15:06:07 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DasHornWebsite::Application.config.secret_token = 'b73d507dc46dc08a7a83e26ed906bba534d120fbc2c5d6577a78b45c35542ef879f8487a206b7789b37cfab72c272140a3bafac0fc630b4a821a1e3617ad4730'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DasHornWebsite::Application.config.session_store :cookie_store, :key => '_Das_Horn_Website_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# DasHornWebsite::Application.config.session_store :active_record_store
